841be56adb2d29a6ecb6cec3b5e86b819730eefd
LukeZhen <duongbaoloc007@gmail.com>
2024-10-24 22:22:24 +0800

    Update question_1.cpp

--- question_1.cpp
+++ question_1.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-    for (i = 1; i<n; i++) {
-        key = arr[i];
-        j=i-1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j+1] = arr[j];
-            j=j-1;
-        }
-        arr[j+1] = key;
+void insertAtBottom(stack<int>& st, int element) {
+    if (st.empty()) {
+        st.push(element);
+        return;
     }
+
+    int topElement = st.top();
+    st.pop();
+    insertAtBottom(st, element);
+    st.push(topElement);
 }
+
+void reverseStack(stack<int>& st) {
+    if (st.empty()) {
+        return;
+    }
+
+    int topElement = st.top();
+    st.pop();
+    reverseStack(st);
+    insertAtBottom(st, topElement);
+}
+
 int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    insertionSort(arr, n);
-    cout << "Sorted array: \n";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    stack<int> st;
+    st.push(28);
+    st.push(29);
+    st.push(30);
+    st.push(31);
+    cout << "Original stack: ";
+    stack<int> tempSt1 = st;
+    while (!tempSt1.empty()) {
+        cout << tempSt1.top() << " ";
+        tempSt1.pop();
+    }
+    cout << endl;
+    reverseStack(st);
+    cout << "Reversed stack: ";
+    stack<int> tempSt2 = st;
+    while (!tempSt2.empty()) {
+        cout << tempSt2.top() << " ";
+        tempSt2.pop();
+    }
     cout << endl;
     return 0;
 }
