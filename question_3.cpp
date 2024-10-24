5ffaaae3b31541f003286d81d500023338b10374
LukeZhen <duongbaoloc007@gmail.com>
2024-10-24 22:42:18 +0800

    Update question_3.cpp

--- question_3.cpp
+++ question_3.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
-
+#include <deque>
 using namespace std;
 
-int main()
-{
-    int Matrix[2][3][2] = {{{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}}};
-    cout << "The 3D matrix is:\n";
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            for (int k = 0; k < 2; k++)
-            {
-                cout << Matrix[i][j][k] << " ";
+void printFirstNegative(int arr[], int n, int k) {
+    deque<int> dq;
+
+    for (int i = 0; i < n; i++) {
+        if (!dq.empty() && dq.front() == i - k) {
+            dq.pop_front();
+        }
+
+        if (arr[i] < 0) {
+            dq.push_back(i);
+        }
+
+        if (i >= k - 1) {
+            cout << "{ ";
+            for (int j = i - k + 1; j <= i; j++) {
+                cout << arr[j] << " ";
+            }
+            cout << "} = ";
+
+            if (!dq.empty()) {
+                cout << arr[dq.front()];
+            } else {
+                cout << "0 (does not contain a negative integer)";
             }
             cout << endl;
         }
-        cout << endl;
     }
+}
+
+int main() {
+    int arr[] = {-8, 2, 3, -6, 10};
+    int k = 2;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "First negative integer for each window of size " << k << endl;
+    printFirstNegative(arr, n, k);
+
     return 0;
 }
