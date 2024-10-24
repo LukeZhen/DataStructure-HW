b10b2b3d9aeee4280ab89dd53ab7582af7861aa9
LukeZhen <duongbaoloc007@gmail.com>
2024-10-24 22:34:28 +0800

    Update question_2.cpp

--- question_2.cpp
+++ question_2.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
-int main() {
-    int Matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    cout << "The matrix is:\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << Matrix[i][j] << " ";
+void printNGE(int arr[], int n) {
+    stack<int> st;
+    int nge[n];
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && arr[st.top()] < arr[i]) {
+            nge[st.top()] = arr[i];
+            st.pop();
         }
-        cout << endl;
+        st.push(i);
     }
+    while (!st.empty()) {
+        nge[st.top()] = -1;
+        st.pop();
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " --> " << nge[i] << endl;
+    }
+}
+int main() {
+    int arr[] = {3,6,10,12,19};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printNGE(arr, n);
     return 0;
 }
